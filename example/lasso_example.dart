d5c82f61f40dcec86da399fd00572470c1996eb0
PointSource <47544779+point-source@users.noreply.github.com>
2021-07-19 14:18:56 -0700

    Update example

--- example/lasso_example.dart
+++ example/lasso_example.dart
@@ -1,6 +1,16 @@
 import 'package:lasso/lasso.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future main() async {
+  final client = Lasso.create(apiKey: 'my-api-key');
+
+  final crewResult = await client.crewList(limit: 10);
+
+  if (crewResult.statusCode != 200) {
+    ///Some network error
+    print(crewResult.error);
+  }
+
+  for (Crew c in crewResult.body?.results ?? []) {
+    print('${c.firstName} ${c.lastName}');
+  }
 }
